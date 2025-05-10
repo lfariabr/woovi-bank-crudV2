144e77d99bc5ec20ddee2531e55af2c302a648b9
Luis Faria <lfariabr@gmail.com>
2025-05-10 10:58:08 +0800

    feat(account-subscriptions): added complete code to be able to listen to new accounts created\n- tried running graphql explorer to test, didn't work\n- next: try running apollo studio or Altair to check\n\ncloses #1, relates #3

--- apps/server/src/modules/account/subscriptions/AccountAddedSubscription.ts
+++ apps/server/src/modules/account/subscriptions/AccountAddedSubscription.ts
@@ -6,6 +6,7 @@ import { Account } from '../accountModel';
 import { redisPubSub } from '../../pubSub/redisPubSub';
 import { PUB_SUB_EVENTS } from '../../pubSub/pubSubEvents';
 import mongoose from 'mongoose';
+import { AccountType } from '../accountType';
 
 type AccountAddedPayload = {
 	account: string;
@@ -13,22 +14,36 @@ type AccountAddedPayload = {
 
 const subscription = subscriptionWithClientId({
 	name: 'AccountAdded',
-    inputFields: {},
 	subscribe: withFilter(
-        () => redisPubSub.asyncIterator(PUB_SUB_EVENTS.ACCOUNT.ADDED),
-        async (payload: AccountAddedPayload, context) => {
-          console.log('accountAdded subscription called with: ', payload);
-          const account = await Account.findOne({
-            _id: new mongoose.Types.ObjectId(payload.account),
-          });
-          return !!account;
+		() => redisPubSub.asyncIterator(PUB_SUB_EVENTS.ACCOUNT.ADDED),
+		async (payload: AccountAddedPayload, context) => {
+			const account = await Account.findOne({
+				_id: new mongoose.Types.ObjectId(payload.account),
+			});
+
+			if (!account) {
+				return false;
+			}
+
+			return true;
 		}
 	),
-	getPayload: async (obj: AccountAddedPayload) => ({
-		account: obj.account,
-	}),
+	getPayload: async (obj: AccountAddedPayload) => {
+		console.log('AccountAddedSubscription getPayload', obj);
+		if (!obj || !obj.account) return {};
+		return { account: obj.account };
+	},
 	outputFields: {
-		...accountField('account'),
+		account: {
+			type: AccountType,
+			resolve: async (obj: any, _, context) => {
+				console.log('account resolve', obj);
+				if (!obj || !obj.account) return null;
+				return await Account.findOne({
+				  _id: new mongoose.Types.ObjectId(obj.account),
+				});
+			},
+			},
 	},
 });
 
